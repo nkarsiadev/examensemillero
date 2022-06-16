6d4c706e3f64ebd9c67fdbfbb28f5089f8d86fb8
alex-vinicio <alexandino2011@gmail.com>
2022-06-16 09:47:15 -0500

    first template of backend

---
+++ pruebaback/src/main/java/com/banco/pichincha/pruebaback/enities/Roles.java
@@ -0,0 +1,5 @@
+package com.banco.pichincha.pruebaback.enities;
+
+public class Roles {
+
+}
---
+++ pruebaback/src/test/java/com/banco/pichincha/pruebaback/PruebabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.pichincha.pruebaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
